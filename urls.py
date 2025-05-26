59e2e33c9a716c4337d1288118f80961366b7197
Rimsha-Katoch <katochrimsha@gmail.com>
2025-05-26 17:24:49 +0530

    Add files via upload

--- urls.py
+++ urls.py
@@ -1,10 +1,29 @@
-from django.urls import path
-from . import views
+"""
+URL configuration for suspect_detection project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
-    path('', views.home, name='home'),
-    path('results/', views.results, name='results'),
-    path('analytics/', views.analytics, name='analytics'),
-    path('clear/', views.clear_session, name='clear_session'),
-    path('delete-last-50/', views.delete_last_50_records, name='delete_last_50_records'),
+    path('admin/', admin.site.urls),
+    path('', include('analyzer.urls')),  # <--- this is critical
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
